de602228753e608e5972dbbe6685f5b46a79c995
Zak Kolar <zak.kolar@gmail.com>
2017-08-21 00:27:36 -0400

    logger includes URL

--- trainer/activity/log-activity.ts
+++ trainer/activity/log-activity.ts
@@ -2,6 +2,9 @@ import {TrainerServerQueue} from "../sync/trainer-server-queue";
 import {Timestamp} from "../helpers/timestamp";
 
 export const LogActivity = function(type, data){
+    data = Object.assign(data, {
+       url: window.location.href
+    });
     TrainerServerQueue.addJob('local_inlinetrainer_log_activity', {
         timestamp: Timestamp(),
         type: type,
