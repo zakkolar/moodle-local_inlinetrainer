06e346e5b2949e03ab88461a08ccfcd5c842ff89
Zak Kolar <zak.kolar@gmail.com>
2017-10-11 18:14:18 -0400

    cleaned up usetrainer capability

--- db/access.php
+++ db/access.php
@@ -1,14 +1,9 @@
 <?php
 $capabilities = array(
     'local/inlinetrainer:usetrainer' => array(
-        'riskbitmask' => RISK_PERSONAL,
         'captype' => 'write',
         'contextlevel' => CONTEXT_SYSTEM,
         'archetypes' => array(
-            'teacher' => CAP_ALLOW,
-            'editingteacher' => CAP_ALLOW,
-            'coursecreator' => CAP_ALLOW,
-            'manager' => CAP_ALLOW,
         )
     ),
 );
