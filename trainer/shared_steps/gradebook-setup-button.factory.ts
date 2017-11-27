947591d2afc4400e8b29bdbad882310c49fd2ad7
Zak Kolar <zak.kolar@gmail.com>
2017-11-27 00:49:10 -0500

    adjust for edit mode in GradebookSetupButtonFactory

--- trainer/shared_steps/gradebook-setup-button.factory.ts
+++ trainer/shared_steps/gradebook-setup-button.factory.ts
@@ -1,11 +1,13 @@
 import {ShowHint} from "../helpers/show-hint";
 import {RouteStep} from "../step/route-step";
 import {StepFactory} from "./step-factory";
+import {CheckEditingModeOn} from "../helpers/check-editing-mode-on";
 
 export const GradebookSetupButtonFactory = StepFactory(RouteStep, {
     text: 'Click "Gradebook setup"',
     help: function(){
-        ShowHint('#settingsnav>ul>.type_course.contains_branch>ul>li:nth-of-type(9)');
+        const n = CheckEditingModeOn() ? 9 : 8;
+        ShowHint('#settingsnav>ul>.type_course.contains_branch>ul>li:nth-of-type('+n+')');
     },
     route: '/grade/edit/tree/index.php',
     identifier:'gradebook_setup_button'
