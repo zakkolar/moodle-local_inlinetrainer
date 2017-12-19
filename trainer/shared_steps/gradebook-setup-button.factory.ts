09c92b50f63c83071ad3eb9ccc8a303aaad8dc41
Zak Kolar <zak.kolar@gmail.com>
2017-12-19 11:20:53 -0600

    fixed gradebook setup factory hint

--- trainer/shared_steps/gradebook-setup-button.factory.ts
+++ trainer/shared_steps/gradebook-setup-button.factory.ts
@@ -6,8 +6,8 @@ import {CheckEditingModeOn} from "../helpers/check-editing-mode-on";
 export const GradebookSetupButtonFactory = StepFactory(RouteStep, {
     text: 'Click "Gradebook setup"',
     help: function(){
-        const n = CheckEditingModeOn() ? 9 : 8;
-        ShowHint('#settingsnav>ul>.type_course.contains_branch>ul>li:nth-of-type('+n+')');
+        const n = CheckEditingModeOn() ? 8 : 7;
+        ShowHint('#settingsnav>ul>.type_course.contains_branch>ul>li:nth-of-type(7)');
     },
     route: '/grade/edit/tree/index.php',
     identifier:'gradebook_setup_button'
