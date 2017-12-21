7233937c1b04afc68eef6c0cde858b6002e6bf8b
Zak Kolar <zak.kolar@gmail.com>
2017-12-21 00:06:47 -0600

    fixed string comparison error

--- trainer/helpers/check-attribute.ts
+++ trainer/helpers/check-attribute.ts
@@ -1,5 +1,5 @@
 import {ItemExists} from './item-exists';
 export const CheckAttribute = function(target: string, attribute: string, value:string, invert = false) {
   const $ = require('jquery');
-  return ItemExists(target) && (invert ? $(target).attr(attribute) !== value : $(target).attr(attribute) === value);
+  return ItemExists(target) && (invert ? $(target).attr(attribute).toString() !== value : $(target).attr(attribute).toString() === value);
 };
