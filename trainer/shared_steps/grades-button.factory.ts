5893c245624f21e48f05d43de7101d4dec6e3571
Zak Kolar <zak.kolar@gmail.com>
2017-12-21 12:46:23 -0600

    fixed GradesButtonFactory

--- trainer/shared_steps/grades-button.factory.ts
+++ trainer/shared_steps/grades-button.factory.ts
@@ -6,8 +6,7 @@ import {CheckEditingModeOn} from "../helpers/check-editing-mode-on";
 export const GradesButton = StepFactory(RouteStep, {
     text: 'Click "Grades"',
     help: function(){
-        const n = CheckEditingModeOn() ? 8 : 7;
-        ShowHint('#settingsnav>ul>.type_course.contains_branch>ul>li:nth-of-type('+n+')');
+        ShowHint('.block_navigation ul .type_system.contains_branch ul li ul li:nth-of-type(4)');
     },
     route: '/grade/report/grader/index.php',
     identifier:'grades_button'
