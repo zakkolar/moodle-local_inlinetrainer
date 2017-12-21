3d58f7c3ccf280c561d4a557c343012fe1934db3
Zak Kolar <zak.kolar@gmail.com>
2017-12-21 16:16:07 -0600

    removed video from assignments and gradebook

--- version.php
+++ version.php
@@ -22,6 +22,6 @@
  
 defined('MOODLE_INTERNAL') || die();
  
-$plugin->version   = 2017122101;
+$plugin->version   = 2017122102;
 $plugin->requires  = 2016052305;
 $plugin->component = 'local_inlinetrainer';
